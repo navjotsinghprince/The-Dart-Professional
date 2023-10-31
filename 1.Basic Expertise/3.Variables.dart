bc31028f015d6f2e93576f6379a41911318e3b02
navjotsinghprince <info@navjotsinghprince.com>
2023-10-31 19:31:55 +0530

    The Dart Professional

--- 1.Basic Expertise/3.Variables.dart
+++ 1.Basic Expertise/3.Variables.dart
@@ -1,9 +1,9 @@
-// String: For storing text value. E.g. “John” [Must be in quotes]
-// int: For storing integer value. E.g. 10, -10, 8555 [Decimal is not included]
-// double: For storing floating point values. E.g. 10.0, -10.2, 85.698 [Decimal is included]
-// num: For storing any type of number. E.g. 10, 20.2, -20 [both int and double]
+// String: For storing text value. E.g. “Prince” [Must be in quotes]
+// int: For storing integer value. E.g. 10, -33, 23099 [Decimal is not included]
+// double: For storing floating point values. E.g. 10.0, -11.2, 50.399 [Decimal is included]
+// num: For storing any type of number. E.g. 10, 21.5, -27 [both int and double]
 // bool: For storing true or false. E.g. true, false [Only stores true or false values]
-// var: For storing any value. E.g. ‘Bimal’, 12, ‘z’, true
+// var: For storing any value. E.g. “Prince”, 15, ‘x’, true
 
 String name = "Prince";
 int number = 05;
@@ -19,7 +19,7 @@ Object myname = 'Bob';
 
 //Null safety
 //String first_name;  // Non-nullable type. Cannot be `null` but can be string. , Throw ERROR
-String? last_name; // Nullable type. Can be `null` or string.
+String? last_name;    // Nullable type. Can be `null` or string.
 
 void main() {
   print("My name is " + name);
